9d0aab4e34e7150d8e9fa5c5b44c849b465310bd
John Bellamy <john.devon.bellamy@gmail.com>
2018-12-22 00:16:24 -0500

    added usage examples

---
+++ examples/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"reflect"
+
+	"github.com/sirupsen/logrus"
+	. "gitlab.com/jdbellamy/kundalini"
+)
+
+func init() {
+	textFmt := new(logrus.TextFormatter)
+	textFmt.DisableTimestamp = true
+	logrus.SetFormatter(textFmt)
+	logrus.SetLevel(logrus.DebugLevel)
+}
+
+func main() {
+
+	buf := []int{}
+	ptr := reflect.ValueOf(&buf)
+
+	v := []int{0, 1, 2, 3, 4}
+
+	kn, err := Wrap(v).
+		Filter(even).
+		Map(double).
+		Export(ptr).
+		Filter(firstN(1)).
+		Concat(Wrap(buf).
+			Filter(firstN(1)).
+			ReleaseOrPanic()).
+		Reduce(8, sum).
+		Types().
+		Release()
+
+	if err != nil {
+		logrus.Errorln(err)
+	}
+
+	logrus.Infoln("... kn:", kn)
+}
+
+func even(x interface{}) bool {
+	return x.(int)%2 == 0
+}
+
+func double(x interface{}) interface{} {
+	return x.(int) * 2
+}
+
+func sum(acc interface{}, x interface{}) interface{} {
+	return acc.(int) + x.(int)
+}
+
+func firstN(n interface{}) func(interface{}) bool {
+	count := 0
+	return func(interface{}) bool {
+		var r = true
+		if count < n.(int) {
+			r = false
+		}
+		count = count + 1
+		return r
+	}
+}
